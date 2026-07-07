e37bccf25f640809eb006f9dd47968740efe312d
Jiveetesh Mourya <jcodestudy@gmail.com>
2026-07-07 10:55:47 +0530

    connect backend and frontend

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,1 +1,2 @@
-export const Backend_Url = "http://localhost:5000/api/v1";
+export const Backend_Url = "https://ai-persona-flax-eight.vercel.app/api/v1";
+// export const Backend_Url = "http://localhost:5000/api/v1";
